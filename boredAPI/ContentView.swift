93571cf8ad1d1f6b5a1e762e7ca60efe481bbacd
Algenis <algenis.jo@gmail.com>
2022-06-15 16:29:59 -0400

    Initial Commit

---
+++ boredAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  boredAPI
+//
+//  Created by Algenis on 6/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ boredAPI/boredAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  boredAPIApp.swift
+//  boredAPI
+//
+//  Created by Algenis on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct boredAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
